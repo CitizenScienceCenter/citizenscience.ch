b7793410d7991027d7128b16f1b6bdca697dc0ab
Marcel Tanner <marcel.tanner@uzh.ch>
2018-10-09 21:33:08 +0200

    cover

--- src/components/Cover.vue
+++ src/components/Cover.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="cover">
+    <p>PEOPLE/RESEARCH TITLE</p>
+    <div class="cover-overlay">
+    </div>
   </div>
 </template>
 
@@ -14,8 +17,35 @@ export default {
 @import '@/colors.scss';
 
 .cover {
-  height: 560px;
-  background-image: linear-gradient(to bottom right, $color-primary, $color-secondary);
+  height: 640px;
+  background-image: url('../assets/sylwia-bartyzel-3149-unsplash.jpg');
+  background-size: cover;
+  background-position: 50% 50%;
+  position: relative;
+
+  p {
+    position: relative;
+    color: white;
+    z-index: 1;
+    text-align: center;
+    margin: auto;
+    padding-top: 200px;
+    font-size: 61px;
+    line-height: 1;
+    font-weight: 700;
+  }
+
+  .cover-overlay {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: block;
+    width: 100%;
+    height: 100%;
+    background-image: linear-gradient(to bottom right, $color-secondary, $color-primary);
+    opacity: 0.8;
+  }
 }
 
 </style>

6a309229dec8871950fc2d3af1397c3d51b9f1cb
Marcel Tanner <marcel.tanner@uzh.ch>
2019-04-15 11:06:19 +0200

    path issue (wip)

--- src/router/router.js
+++ src/router/router.js
@@ -5,6 +5,7 @@ import store from '../store/store.js';
 export const router = new VueRouter({
   routes: routes,
   mode: 'history',
+    pathToRegexpOptions: { strict: false },
   scrollBehavior (to, from, savedPosition) {
     return { x: 0, y: 0 }
   }
